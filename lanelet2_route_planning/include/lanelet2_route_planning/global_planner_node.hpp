f5e74ec717917d6ee1c50e49ff3199b2c4a17897
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2022-12-28 17:30:14 +0100

    started implementing ll2_map_interface, WIP

--- lanelet2_route_planning/include/lanelet2_route_planning/global_planner_node.hpp
+++ lanelet2_route_planning/include/lanelet2_route_planning/global_planner_node.hpp
@@ -1,13 +1,16 @@
 #include "rclcpp/rclcpp.hpp"
 
+#include "lanelet2_map_interface/lanelet2_map_interface.hpp"
+
 using namespace std::chrono_literals;
 
 class GlobalPlanner : public rclcpp::Node
 {
     public:
         GlobalPlanner();
+        void initializeMapInterface();
+        bool map_if_initialized_ = false;
         
     private:
-        void timer_callback();
-        rclcpp::TimerBase::SharedPtr timer_;
+        LL2MapInterface *ll2if_;
 };
--- lanelet2_route_planning/src/global_planner_node.cpp
+++ lanelet2_route_planning/src/global_planner_node.cpp
@@ -2,18 +2,36 @@
 
 GlobalPlanner::GlobalPlanner() : Node("global_planner")
 {
-  timer_ = this->create_wall_timer(500ms, std::bind(&GlobalPlanner::timer_callback, this));
+
 }
 
-void GlobalPlanner::timer_callback()
+void GlobalPlanner::initializeMapInterface()
 {
-  RCLCPP_INFO(this->get_logger(), "Hello World!");
+  // To-Do load name as parameter
+  std::string map_server_name = "ll2_map_server";
+  // Important: shared_from_this() can not be called from within the constructor
+  ll2if_ = new LL2MapInterface(shared_from_this(), map_server_name);
 }
 
 int main(int argc, char ** argv)
 {
+  RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Start");
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<GlobalPlanner>());
+  RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Init Ready");
+  auto planner = std::make_shared<GlobalPlanner>();
+  RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Created Node Pointer");
+
+  while(rclcpp::ok())
+  {
+    rclcpp::spin_some(planner);
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Spinned once!");
+    if(!planner->map_if_initialized_)
+    {
+      planner->initializeMapInterface();
+      RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Initialized Map Interface");
+    }
+  }
+  
   rclcpp::shutdown();
   return 0;
 }
